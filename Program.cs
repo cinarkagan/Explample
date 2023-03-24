8144fc8d5e84d1b7cccd0b12874d73b41d40dcc9
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2023-03-24 17:23:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Explample // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
